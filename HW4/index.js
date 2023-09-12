87e859c46cfcdc86c4670eacc280a995f2cd579b
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-09-12 19:14:20 +0700

    Add looping on HW4

--- HW4/index.js
+++ HW4/index.js
@@ -1,17 +1,16 @@
-// Array Pertama
-const array = [
-  21, 11, 32, 49, 43, 40, 26, 27, 27, 34, 25, 6, 38, 2, 33, 17, 21, 5, 47, 15,
-  21, 17, 9, 49, 38, 3, 1, 26, 17, 5, 11, 40, 44, 4, 1, 5, 35, 20, 21, 23, 23,
-  27, 7, 20, 10, 4, 36, 21, 16, 40, 27, 5, 47, 11, 15, 35, 49, 25, 34, 16, 19,
-  43, 2, 25, 5, 25, 32, 13, 38, 30, 7, 36, 9, 30, 11, 22, 35, 42, 21, 2, 40, 20,
-  15, 13, 21, 21, 36, 41, 11, 41, 44, 15, 11, 23, 15, 30, 2, 16, 20, 47,
-];
-// Array Pecahan
+// Variables Array
+let array = [];
 let oddArray = [];
 let evenArray = [];
 
+// Perulangan untuk menghasilkan nilai array random
+for (let i = 0; i < 100; i++) {
+  let value = Math.floor(Math.random() * 50) + 1;
+  array.push(value);
+}
+
 // Fungsi Membagi array berdasarkan indeks
-const generateArray = (item, index) => {
+const separateArray = (item, index) => {
   if (index % 2 === 0) {
     evenArray.push(item);
   } else {
@@ -19,7 +18,7 @@ const generateArray = (item, index) => {
   }
 };
 
-// Fungsi Mencari nilai terkecil dan terbesar
+// // Fungsi Mencari nilai terkecil dan terbesar
 const getMaxValue = (...items) => {
   let maxValue = 0;
   for (let i = 0; i < items.length; i++) {
@@ -40,7 +39,7 @@ const getMinValue = (...items) => {
   return minValue;
 };
 
-// Fungsi Mencari Nilai total dan rata rata
+// // Fungsi Mencari Nilai total dan rata rata
 const getTotal = (total, num) => {
   return total + num;
 };
@@ -49,7 +48,7 @@ const getAverage = (total, ...array) => {
   return total / array.length;
 };
 
-// Fungsi Perbandingan
+// // Fungsi Perbandingan
 const compare = (odd, even) => {
   if (odd === even) {
     return "NILAI ARRAY GANJIL 'SAMA DENGAN' NILAI ARRAY GENAP";
@@ -60,19 +59,19 @@ const compare = (odd, even) => {
   }
 };
 
-//Membagi array berdasarkan index ganjil dan genap
-array.forEach(generateArray);
-// Variabel untuk menyimpan nilai MAX dan MIN
+// //Membagi array berdasarkan index ganjil dan genap
+array.forEach(separateArray);
+// // Variabel untuk menyimpan nilai MAX dan MIN
 const maxValueOddArray = getMaxValue(...oddArray);
 const minValueOddArray = getMinValue(...oddArray);
 const maxValueEvenArray = getMaxValue(...evenArray);
 const minValueEvenArray = getMinValue(...evenArray);
-// Variabel untuk menyimpan nilai total dan rata - rata
+// // Variabel untuk menyimpan nilai total dan rata - rata
 const totalOddArray = oddArray.reduce(getTotal);
 const averageOddArray = getAverage(totalOddArray, ...oddArray);
 const totalEvenArray = evenArray.reduce(getTotal);
 const averageEvenArray = getAverage(totalEvenArray, ...evenArray);
-// Variabel untuk menyimpan nilai perbandingan
+// // Variabel untuk menyimpan nilai perbandingan
 const maxComparison = compare(maxValueOddArray, maxValueEvenArray);
 const minComparison = compare(minValueOddArray, minValueEvenArray);
 const totalComparison = compare(totalOddArray, totalEvenArray);
