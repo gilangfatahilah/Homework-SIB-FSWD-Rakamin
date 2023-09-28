697894a827e77e4b414f34d0784fee82c00ef4bd
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-09-28 22:50:00 +0700

    Add folder HW7

---
+++ HW7/planeFigureCounter.js
@@ -0,0 +1,14 @@
+exports.count = {
+  squareArea: function (side) {
+    return "Square Area is : " + side ** 2;
+  },
+  squarePerimeter: function (side) {
+    return "Square Perimeter is : " + side * 4;
+  },
+  rectangleArea: function (width, length) {
+    return "Rectangle Area is : " + width * length;
+  },
+  rectanglePerimeter: function (width, length) {
+    return "Rectangle Perimeter is : " + (width + length) * 2;
+  },
+};
---
+++ HW7/soal1.js
@@ -0,0 +1,8 @@
+// Import local file
+const plainFigure = require("./planeFigureCounter");
+
+// Show output
+console.log(plainFigure.count.squareArea(20));
+console.log(plainFigure.count.squarePerimeter(20));
+console.log(plainFigure.count.rectangleArea(5, 10));
+console.log(plainFigure.count.rectanglePerimeter(5, 10));
