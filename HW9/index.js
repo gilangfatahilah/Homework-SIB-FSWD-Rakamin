ea37bf8b923caa9e91b91ce8de5cf13501aaf458
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-10-15 20:34:32 +0700

    add: docs folder for swagger configs

--- HW9/index.js
+++ HW9/index.js
@@ -1,42 +1,26 @@
+// package
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const router = require("./src/routes");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
+// routers
+const router = require("./src/routes");
+app.use("/", router);
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "REST API Movies Documentation",
-      version: "1.0.0",
-      description:
-        "this is the REST API Movies documentation, this api documentation is created due my homework project on Rakamin Academy.",
-    },
-    externalDocs: {
-      description: "Find out more about Swagger",
-      url: "https://swagger.io",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000/",
-      },
-    ],
-  },
-  apis: ["./src/routes/*"],
-};
-const specs = swaggerJsdoc(options);
+// swagger configuration
+const swaggerConfig = require("./docs/swagger.json");
+const specs = swaggerJsdoc(swaggerConfig);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
+// middleware
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.use("/", router);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-
+// port configuration
 app.listen(
   process.env.PORT,
   console.log(`app listening on PORT : ${process.env.PORT}`)
