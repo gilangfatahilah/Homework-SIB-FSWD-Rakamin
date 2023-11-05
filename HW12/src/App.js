3ed60e7d59f97005998f30f4181334b7d512662c
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-11-05 22:27:59 +0700

    finish the functions

--- HW12/src/App.js
+++ HW12/src/App.js
@@ -1,43 +1,61 @@
-
-import * as React from 'react';
+import * as React from "react";
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [nextValue, setNextValue] = React.useState(calculateNextValue(squares));
+  const winner = calculateWinner(squares);
 
+  function selectSquare(square) {
+    if (squares[square] || winner) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[square] = nextValue;
+    setSquares(newSquares);
+    setNextValue(calculateNextValue(newSquares));
   }
 
   function restart() {
+    setSquares(Array(9).fill(null));
+    setNextValue(calculateNextValue(Array(9).fill(null)));
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button
+        className="bg-orange-200 hover:bg-orange-300 m-1 w-16 h-16 text-orange-400 font-bold after:absolute"
+        onClick={() => selectSquare(i)}
+      >
         {squares[i]}
       </button>
     );
   }
 
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
+    <div className=" flex flex-col items-center mt-20">
+      <h1 className="text-xl font-bold mb-5 text-orange-400">
+        STATUS: {calculateStatus(winner, squares, nextValue)}
+      </h1>
+      <div>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div >
+      <div>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div >
+      <div>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button onClick={restart}>
-        restart
+      <button
+        onClick={restart}
+        className="mt-5 py-2 px-5 bg-orange-400 rounded-md font-medium"
+      >
+        Restart
       </button>
     </div>
   );
@@ -45,8 +63,8 @@ function Board() {
 
 function Game() {
   return (
-    <div >
-      <div >
+    <div>
+      <div>
         <Board />
       </div>
     </div>
@@ -58,13 +76,13 @@ function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`;
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+  return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
